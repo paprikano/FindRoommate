951631fcb3bcf3c4997b680311a89000b35c534a
paprikano <p.domanski92@gmail.com>
2020-02-27 11:21:41 +0100

    Add files via upload

--- Models/AdvertImage/EFAdvertImageRepository.cs
+++ Models/AdvertImage/EFAdvertImageRepository.cs
@@ -3,18 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using FindRoommate.Infrastructure;
+using Microsoft.AspNetCore.Http;
 
 namespace FindRoommate.Models.AdvertImage
 {
     public class EFAdvertImageRepository : IAdvertImageRepository
     {
         private readonly ApplicationDbContext context;
+        private readonly IImageService imageService;
 
-        public EFAdvertImageRepository(ApplicationDbContext context)
+        public EFAdvertImageRepository(ApplicationDbContext context, IImageService imageService)
         {
             this.context = context;
+            this.imageService = imageService;
         }
 
         public IQueryable<AdvertImage> AdvertImages => context.AdvertImages;
+
+        public void UploadAdvertImages(List<IFormFile> files, int advertId)
+        {
+            foreach (IFormFile file in files)
+            {
+                imageService.UploadImage(file);
+                AdvertImage advertImage = new AdvertImage()
+                {
+                    ImagePath = file.FileName,
+                    AdvertId = advertId
+                };
+
+                context.AdvertImages.Add(advertImage);
+            }
+
+            context.SaveChanges();
+        }
+
+        public void DeleteAdvertImage(int advertImageId)
+        {
+            AdvertImage advertImage = AdvertImages.FirstOrDefault(a => a.AdvertImageId == advertImageId);
+            if (advertImage != null)
+            {
+                context.Remove(advertImage);
+                context.SaveChanges();
+            }
+        }        
     }
 }
--- Models/AdvertImage/IAdvertImageRepository.cs
+++ Models/AdvertImage/IAdvertImageRepository.cs
@@ -2,11 +2,16 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 
 namespace FindRoommate.Models.AdvertImage
 {
     public interface IAdvertImageRepository
     {
         IQueryable<AdvertImage> AdvertImages { get; }
+
+        void UploadAdvertImages(List<IFormFile> files, int advertId);
+
+        void DeleteAdvertImage(int advertImageId);
     }
 }
