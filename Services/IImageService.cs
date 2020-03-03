941778855425011efc77f50f9172cf2e67ab46d2
paprikano <p.domanski92@gmail.com>
2020-03-03 14:25:38 +0100

    Add files via upload

---
+++ Services/IImageService.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindRoommate.Services
+{
+    public interface IImageService
+    {
+        void UploadImage(IFormFile file);
+    }
+}
